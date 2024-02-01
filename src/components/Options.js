e2526c323549f7bd4fcbc1bd08c050f1d6335cf7
melikaknight <38429357+melikaknight@users.noreply.github.com>
2024-01-31 23:53:14 -0500

    Add files via upload

---
+++ src/components/Options.js
@@ -0,0 +1,18 @@
+function Options({ question, dispatch, answer }) {
+  return (
+    <div className="options">
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""}${
+            index === question.correctOption ? "correct" : "wrong"
+          }`}
+          key={option}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
+export default Options;
